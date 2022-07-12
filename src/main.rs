ba0979976a125d6bb8509567bce7c37fe33b10f7
Edmundo Paulino <manyymoore@gmail.com>
2022-07-12 13:58:43 +0000

    refactor: 🎨 message passing structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::sync::mpsc::channel;
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let (tx, rx) = channel();
+    for i in 0..10 {
+        let tx = tx.clone();
+        thread::spawn(move || {
+            tx.send(i).unwrap();
+        });
+    }
+
+    for _ in 0..10 {
+        let j = rx.recv().unwrap();
+        assert!((0..10).contains(&j));
+    }
 }
